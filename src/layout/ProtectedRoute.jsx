67085b2d7dbedf36e6353103c0dfcf6a3d2c919e
Leglocker01 <beristaindelamora@gmail.com>
2024-01-04 20:24:03 -0600

    adding a spinner

--- src/layout/ProtectedRoute.jsx
+++ src/layout/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { Spinner } from "../components/Spinner";
 
 export default function ProtectedRoute() {
   const { auth, isCharging } = useAuth();
   // spinner
-  if (isCharging) return "Cargando...";
+  if (isCharging) return <Spinner />;
   return (
     <>
       {auth ? (
