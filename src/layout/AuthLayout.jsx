dbd37340c09c793d0f4b3a8fa1d1f30129de5e63
Leglocker01 <beristaindelamora@gmail.com>
2024-01-06 21:28:00 -0600

    adding service worker and button to download PWA

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
+  // download the PWA
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+
+  }, []);
+
+  // button for instalation handler
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no saved at window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
   return (
     <>
       <h1>PsychoTracker</h1>
+      {isReadyForInstall && (
+        <button onClick={downloadApp} className="btn-PWA">
+          Descargar App
+        </button>
+      )}
       <Outlet />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import './styles/styles.css'
+import './styles/styles.css';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <App />
   </>
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // code for new app version
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({type: "SKIP_WAITING"});
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
