b8965abb5d6c03d247aef80d1cfa7599e19a5728
Leglocker01 <beristaindelamora@gmail.com>
2024-01-30 12:41:21 -0600

    adding brain model

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,36 +1,58 @@
 // ? three vanilla
-import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { useEffect, useRef } from 'react';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// ... (importaciones)
 export const ModelViewer = () => {
   const mountRef = useRef(null);
 
   useEffect(() => {
     const CurrentMount = mountRef.current;
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-
+    const camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 90;
+    
     const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth * 0.5, window.innerHeight);
+    
+    const ambientLight = new THREE.AmbientLight(0xfefefe, 1);
+    ambientLight.position.set(1, 1, 2);
+    ambientLight.intensity = 1;
+    scene.add(ambientLight);
 
-    const light = new THREE.PointLight(0xFFFFFF);
-    light.position.set(5, 5, 5);
-    scene.add(light);
+    const directionLigth = new THREE.DirectionalLight(0xfefefe);
+    directionLigth.position.set(1, 2, 2);
+    directionLigth.intensity = 3;
+    scene.add(directionLigth);
 
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
-    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00, flatShading: true });
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
+    // model
+    const loader = new GLTFLoader();
+    loader.load("/brain/scene.gltf", (gltf) => {
+      const model = gltf.scene;
+      model.position.y = -15;
+      model.scale.set(0.3, 0.3, 0.3);
+      scene.add(model);
+    });
 
     CurrentMount.appendChild(renderer.domElement);
 
+    // Orbit Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.screenSpacePanning = false;
+    controls.maxPolarAngle = Math.PI / 2;
+
     const animate = () => {
       requestAnimationFrame(animate);
-
-      sphere.rotation.x += 0.01;
-      sphere.rotation.y += 0.01;
-
+      controls.update(); // Actualizar controles en cada cuadro
       renderer.render(scene, camera);
     };
 
@@ -38,8 +60,10 @@ export const ModelViewer = () => {
 
     return () => {
       CurrentMount.removeChild(renderer.domElement);
+      // Limpieza de recursos cuando el componente se desmonta
+      scene.dispose();
     };
   }, []);
 
-  return <div ref={mountRef} style={{ width: '100%', height: '100vh' }} />;
+  return <div className="model" ref={mountRef} />;
 };
